926b5b2fd0334a5dade679ac3eef66c4ce88ce80
Jan Bader <jan.bader@ub-software.de>
2017-06-14 16:07:52 +0200

    Add BranchName to Build

--- TeamCitySharp/DomainEntities/Build.cs
+++ TeamCitySharp/DomainEntities/Build.cs
@@ -4,24 +4,23 @@ namespace TeamCitySharp.DomainEntities
 {
     public class Build
     {
+        public Agent Agent { get; set; }
+        public string BranchName { get; set; }
+        public BuildConfig BuildConfig { get; set; }
+        public string BuildTypeId { get; set; }
+        public ChangeWrapper Changes { get; set; }
+        public DateTime FinishDate { get; set; }
+        public string Href { get; set; }
         public string Id { get; set; }
         public string Number { get; set; }
-        public string Status { get; set; }
-        public string BuildTypeId { get; set; }
-        public string Href { get; set; }
-        public string WebUrl { get; set; }
-        public string StatusText { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime FinishDate { get; set; }
-
-        public BuildConfig BuildConfig { get; set; }
-        public Agent Agent { get; set;}
-        public ChangeWrapper Changes { get; set; }
+        public string Status { get; set; }
+        public string StatusText { get; set; }
+        public string WebUrl { get; set; }
 
         public override string ToString()
         {
             return Number;
         }
-        
     }
 }
