cf4a13710439dadb990397ff06c52daab1d83bc6
Jan Bader <jan.bader@ub-software.de>
2017-06-14 16:08:37 +0200

    Reorganize

--- TeamCitySharp/Connection/TeamCityCaller.cs
+++ TeamCitySharp/Connection/TeamCityCaller.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.IO;
 using System.Net;
 using System.Security.Authentication;
@@ -23,6 +23,23 @@ namespace TeamCitySharp.Connection
             _configuration.HostName = hostName;
         }
 
+        public bool Authenticate(string urlPart)
+        {
+            try
+            {
+                var httpClient = CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.TextPlain);
+                httpClient.ThrowExceptionOnHttpError = true;
+                httpClient.Get(CreateUrl(urlPart));
+
+                var response = httpClient.Response;
+                return response.StatusCode == HttpStatusCode.OK;
+            }
+            catch (HttpException exception)
+            {
+                throw new AuthenticationException(exception.StatusDescription);
+            }
+        }
+
         public void Connect(string userName, string password, bool actAsGuest)
         {
             _configuration.Password = password;
@@ -30,34 +47,25 @@ namespace TeamCitySharp.Connection
             _configuration.ActAsGuest = actAsGuest;
         }
 
-        public T GetFormat<T>(string urlPart, params object[] parts)
+        public void Delete(string urlPart)
         {
-            return Get<T>(string.Format(urlPart, parts));
-        }
-
-        public void GetFormat(string urlPart, params object[] parts)
-        {
-            Get(string.Format(urlPart, parts));
-        }
-
-        public T PostFormat<T>(object data, string contenttype, string accept, string urlPart, params object[] parts)
-        {
-            return Post<T>(data.ToString(), contenttype, string.Format(urlPart, parts), accept);
-        }
-
-        public void PostFormat(object data, string contenttype, string urlPart, params object[] parts)
-        {
-            Post(data.ToString(), contenttype, string.Format(urlPart, parts), string.Empty);
-        }
-
-        public void PutFormat(object data, string contenttype, string urlPart, params object[] parts)
-        {
-            Put(data.ToString(), contenttype, string.Format(urlPart, parts), string.Empty);
+            MakeDeleteRequest(urlPart);
         }
 
         public void DeleteFormat(string urlPart, params object[] parts)
         {
             Delete(string.Format(urlPart, parts));
+        }
+
+        public T Get<T>(string urlPart)
+        {
+            var response = GetResponse(urlPart);
+            return response.StaticBody<T>();
+        }
+
+        public void Get(string urlPart)
+        {
+            GetResponse(urlPart);
         }
 
         public void GetDownloadFormat(Action<string> downloadHandler, string urlPart, params object[] parts)
@@ -66,7 +74,7 @@ namespace TeamCitySharp.Connection
             {
                 throw new ArgumentException("If you are not acting as a guest you must supply userName and password");
             }
-            
+
             if (string.IsNullOrEmpty(urlPart))
             {
                 throw new ArgumentException("Url must be specfied");
@@ -84,7 +92,6 @@ namespace TeamCitySharp.Connection
             {
                 CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.ApplicationJson).GetAsFile(url, tempFileName);
                 downloadHandler.Invoke(tempFileName);
-
             }
             finally
             {
@@ -93,6 +100,71 @@ namespace TeamCitySharp.Connection
                     File.Delete(tempFileName);
                 }
             }
+        }
+
+        public T GetFormat<T>(string urlPart, params object[] parts)
+        {
+            return Get<T>(string.Format(urlPart, parts));
+        }
+
+        public void GetFormat(string urlPart, params object[] parts)
+        {
+            Get(string.Format(urlPart, parts));
+        }
+
+        // only used by the artifact listing methods since i havent found a way to deserialize them into a domain entity
+        public string GetRaw(string urlPart)
+        {
+            if (CheckForUserNameAndPassword())
+                throw new ArgumentException("If you are not acting as a guest you must supply userName and password");
+
+            if (string.IsNullOrEmpty(urlPart))
+                throw new ArgumentException("Url must be specfied");
+
+            var url = CreateUrl(urlPart);
+
+            var httpClient = CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.TextPlain);
+            var response = httpClient.Get(url);
+            if (IsHttpError(response))
+            {
+                throw new HttpException(response.StatusCode, string.Format("Error {0}: Thrown with URL {1}", response.StatusDescription, url));
+            }
+
+            return response.RawText;
+        }
+
+        public T Post<T>(string data, string contenttype, string urlPart, string accept)
+        {
+            return Post(data, contenttype, urlPart, accept).StaticBody<T>();
+        }
+
+        public HttpResponse Post(object data, string contenttype, string urlPart, string accept)
+        {
+            var client = MakePostRequest(data, contenttype, urlPart, accept);
+
+            return client.Response;
+        }
+
+        public T PostFormat<T>(object data, string contenttype, string accept, string urlPart, params object[] parts)
+        {
+            return Post<T>(data.ToString(), contenttype, string.Format(urlPart, parts), accept);
+        }
+
+        public void PostFormat(object data, string contenttype, string urlPart, params object[] parts)
+        {
+            Post(data.ToString(), contenttype, string.Format(urlPart, parts), string.Empty);
+        }
+
+        public HttpResponse Put(object data, string contenttype, string urlPart, string accept)
+        {
+            var client = MakePutRequest(data, contenttype, urlPart, accept);
+
+            return client.Response;
+        }
+
+        public void PutFormat(object data, string contenttype, string urlPart, params object[] parts)
+        {
+            Put(data.ToString(), contenttype, string.Format(urlPart, parts), string.Empty);
         }
 
         public string StartBackup(string urlPart)
@@ -117,15 +189,57 @@ namespace TeamCitySharp.Connection
             return string.Empty;
         }
 
-        public T Get<T>(string urlPart)
+        private static bool IsHttpError(HttpResponse response)
         {
-            var response = GetResponse(urlPart);
-            return response.StaticBody<T>();
+            var num = (int)response.StatusCode / 100;
+
+            return (num == 4 || num == 5);
         }
-        
-        public void Get(string urlPart)
+
+        /// <summary>
+        /// <para>If the <paramref name="response"/> is OK (see <see cref="IsHttpError"/> for definition), does nothing.</para>
+        /// <para>Otherwise, throws an exception which includes also the response raw text.
+        /// This would often contain a Java exception dump from the TeamCity REST Plugin, which reveals the cause of some cryptic cases otherwise showing just "Bad Request" in the HTTP error.
+        /// Also this comes in handy when TeamCity goes into maintenance, and you get back the banner in HTML instead of your data.</para>
+        /// </summary>
+        private static void ThrowIfHttpError(HttpResponse response, string url)
         {
-            GetResponse(urlPart);
+            if (!IsHttpError(response))
+                return;
+            throw new HttpException(response.StatusCode, string.Format("Error: {0}\nHTTP: {3}\nURL: {1}\n{2}", response.StatusDescription, url, response.RawText, response.StatusCode));
+        }
+
+        private bool CheckForUserNameAndPassword()
+        {
+            return !_configuration.ActAsGuest && string.IsNullOrEmpty(_configuration.UserName) && string.IsNullOrEmpty(_configuration.Password);
+        }
+
+        private HttpClient CreateHttpClient(string userName, string password, string accept)
+        {
+            var httpClient = new HttpClient(new TeamcityJsonEncoderDecoderConfiguration());
+            httpClient.Request.Accept = accept;
+            if (!_configuration.ActAsGuest)
+            {
+                httpClient.Request.SetBasicAuthentication(userName, password);
+                httpClient.Request.ForceBasicAuth = true;
+            }
+
+            return httpClient;
+        }
+
+        private string CreateUrl(string urlPart)
+        {
+            var protocol = _configuration.UseSSL ? "https://" : "http://";
+            var authType = _configuration.ActAsGuest ? "/guestAuth" : "/httpAuth";
+
+            return string.Format("{0}{1}{2}{3}", protocol, _configuration.HostName, authType, urlPart);
+        }
+
+        private string GetContentType(string data)
+        {
+            if (data.StartsWith("<"))
+                return HttpContentTypes.ApplicationXml;
+            return HttpContentTypes.TextPlain;
         }
 
         private HttpResponse GetResponse(string urlPart)
@@ -141,47 +255,6 @@ namespace TeamCitySharp.Connection
             var response = CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.ApplicationJson).Get(url);
             ThrowIfHttpError(response, url);
             return response;
-        }
-
-        public T Post<T>(string data, string contenttype, string urlPart, string accept)
-        {
-            return Post(data, contenttype, urlPart, accept).StaticBody<T>();
-        }
-
-        public bool Authenticate(string urlPart)
-        {
-            try
-            {
-                var httpClient = CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.TextPlain);
-                httpClient.ThrowExceptionOnHttpError = true;
-                httpClient.Get(CreateUrl(urlPart));
-
-                var response = httpClient.Response;
-                return response.StatusCode == HttpStatusCode.OK;
-            }
-            catch (HttpException exception)
-            {
-                throw new AuthenticationException(exception.StatusDescription);
-            }
-        }
-
-        public HttpResponse Post(object data, string contenttype, string urlPart, string accept)
-        {
-            var client = MakePostRequest(data, contenttype, urlPart, accept);
-
-            return client.Response;
-        }
-
-        public HttpResponse Put(object data, string contenttype, string urlPart, string accept)
-        {
-            var client = MakePutRequest(data, contenttype, urlPart, accept);
-
-            return client.Response;
-        }
-
-        public void Delete(string urlPart)
-        {
-            MakeDeleteRequest(urlPart);
         }
 
         private void MakeDeleteRequest(string urlPart)
@@ -213,80 +286,6 @@ namespace TeamCitySharp.Connection
             ThrowIfHttpError(client.Response, client.Request.Uri);
 
             return client;
-        }
-
-        private static bool IsHttpError(HttpResponse response)
-        {
-            var num = (int)response.StatusCode / 100;
-
-            return (num == 4 || num == 5);
-        }
-
-        /// <summary>
-        /// <para>If the <paramref name="response"/> is OK (see <see cref="IsHttpError"/> for definition), does nothing.</para>
-        /// <para>Otherwise, throws an exception which includes also the response raw text.
-        /// This would often contain a Java exception dump from the TeamCity REST Plugin, which reveals the cause of some cryptic cases otherwise showing just "Bad Request" in the HTTP error.
-        /// Also this comes in handy when TeamCity goes into maintenance, and you get back the banner in HTML instead of your data.</para> 
-        /// </summary>
-        private static void ThrowIfHttpError(HttpResponse response, string url)
-        {
-            if(!IsHttpError(response))
-                return;
-            throw new HttpException(response.StatusCode, string.Format("Error: {0}\nHTTP: {3}\nURL: {1}\n{2}", response.StatusDescription, url, response.RawText, response.StatusCode));
-        }
-
-        private string CreateUrl(string urlPart)
-        {
-            var protocol = _configuration.UseSSL ? "https://" : "http://";
-            var authType = _configuration.ActAsGuest ? "/guestAuth" : "/httpAuth";
-
-            return string.Format("{0}{1}{2}{3}", protocol, _configuration.HostName, authType, urlPart);
-        }
-
-        private HttpClient CreateHttpClient(string userName, string password, string accept)
-        {
-            var httpClient = new HttpClient(new TeamcityJsonEncoderDecoderConfiguration());
-            httpClient.Request.Accept = accept;
-            if (!_configuration.ActAsGuest)
-            {
-                httpClient.Request.SetBasicAuthentication(userName, password);
-                httpClient.Request.ForceBasicAuth = true;
-            }
-
-            return httpClient;
-        }
-
-        // only used by the artifact listing methods since i havent found a way to deserialize them into a domain entity
-        public string GetRaw(string urlPart)
-        {
-            if (CheckForUserNameAndPassword())
-                throw new ArgumentException("If you are not acting as a guest you must supply userName and password");
-
-            if (string.IsNullOrEmpty(urlPart))
-                throw new ArgumentException("Url must be specfied");
-
-            var url = CreateUrl(urlPart);
-
-            var httpClient = CreateHttpClient(_configuration.UserName, _configuration.Password, HttpContentTypes.TextPlain);
-            var response = httpClient.Get(url);
-            if (IsHttpError(response))
-            {
-                throw new HttpException(response.StatusCode, string.Format("Error {0}: Thrown with URL {1}", response.StatusDescription, url));
-            }
-
-            return response.RawText;
-        }
-
-        private bool CheckForUserNameAndPassword()
-        {
-            return !_configuration.ActAsGuest && string.IsNullOrEmpty(_configuration.UserName) && string.IsNullOrEmpty(_configuration.Password);
-        }
-
-        private string GetContentType(string data)
-        {
-            if (data.StartsWith("<"))
-                return HttpContentTypes.ApplicationXml;
-            return HttpContentTypes.TextPlain;
         }
     }
 }
